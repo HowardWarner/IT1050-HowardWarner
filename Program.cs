f34d0b710029dbde479fc84c25a7d7c405127e77
HowardWarner <43227330+HowardWarner@users.noreply.github.com>
2018-09-19 20:45:17 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IT1050_HowardWarner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! From Howard Warner!");
+            Console.ReadLine();
+        }
+    }
+}
