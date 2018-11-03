feba6a88154d5cfca3ac333fe875147dc907602d
HowardWarner <43227330+HowardWarner@users.noreply.github.com>
2018-11-03 13:30:14 -0400

    Add files via upload

---
+++ 1050 lab wk6.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Week6Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //both of the statements for question 1 work based on condiditons being true or false.
+           // a while statement runs until a false condition is met.
+            /* int speedLimit;
+             int speed;
+
+             speedLimit = 35;
+             speed = 42;
+             if (speed > speedLimit)
+             {
+                 Console.WriteLine("Slow Now");
+             }*/
+            {
+                /* int num01 = 23;
+                 if (num01 == 40)
+                 {
+                     Console.WriteLine("num01 is equal to 40");
+                 }
+                 else
+                     Console.WriteLine("num01 is not equal to 40");
+                 }
+
+                 bool isCold;
+
+                 isCold = false;
+                 if (isCold)
+                 {
+                     Console.WriteLine("It is cold");
+                 }
+                 else
+                 {
+                     Console.WriteLine("It is hot");
+                  }*/
+                /* {
+                     int i = 1;
+                     while (i <= 10)
+                     {
+                         Console.WriteLine(i);
+                         i = i + 1;
+                     }
+                int i;
+                i = 60;
+                while (i >= 20)
+                {
+                    Console.WriteLine(i);
+                    i = i - 1;
+                }*/
+                int i;
+                i = 10;
+                while (i <= 20)
+                {
+                    Console.WriteLine(i);
+                    i = i + 1;
+                }
+                }
+
+
+            }
+        }
+    }
+
