768c29f7c8caef3d056b305ce0402ad1f4f86abc
HowardWarner <43227330+HowardWarner@users.noreply.github.com>
2018-11-20 15:40:21 -0500

    Add files via upload

---
+++ Week 10 Lab.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWK10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            double number1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            double number2 = double.Parse(Console.ReadLine());
+            Console.Write("Enter third: ");
+            double number3 = double.Parse(Console.ReadLine());
+
+            double result = Maximum(number1, number2, number3);
+
+            Console.WriteLine("Maximum is: " + result);
+        }
+        static double Maximum(double x, double y, double z)
+        {
+            double maximumValue = x;
+
+            if (y > maximumValue)
+            {
+                maximumValue = y;
+            }
+            if (z > maximumValue)
+            {
+                maximumValue = z;
+            }
+
+            return maximumValue;
+
+
+
+        }
+    }
+}
