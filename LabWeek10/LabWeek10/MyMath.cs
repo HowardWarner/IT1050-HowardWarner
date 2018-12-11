ea3bbda3aed6c07c293f84e7809698eccf8ad3b1
HowardWarner <43227330+HowardWarner@users.noreply.github.com>
2018-12-11 08:56:35 -0500

    Add files via upload

---
+++ LabWeek10/LabWeek10/MyMath.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyMath
+{
+    public class math
+    {
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("Enter first amount: ");
+            double operand1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter second amount: ");
+            double operand2 = double.Parse(Console.ReadLine());
+            double result = operand1 + operand2;
+            Console.Write("Result is: " + result);
+
+            Console.WriteLine("Maximum is: " + result);
+        }    
+        public static double Maximum(double operand1, double operand2);
+        public double operand1;
+        public double multiply = operand1 * operand2;
+        public double Divide = operand1 / operand2;
+        public double add = operand1 + operand2;
+        public double subtract = operand1 - operand2;
+        private static readonly operand1 operand2;
+
+        double Result { get; }
+        public double Subtract { get => subtract; set => subtract = value; }
+        public double Multiply { get => multiply; set => multiply = value; }
+    }
+
+    internal class operand1
+    {
+    }
+}
+public class Operand1
+{
+}
+
+internal class Multiply
+{
+}
+
---
+++ LabWeek10/LabWeek10/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWeek10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter first number: ");
+            double operand1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            double operand2 = double.Parse(Console.ReadLine());
+           
+
+            double result = Maximum(operand1, operand2);
+
+            Console.WriteLine("Maximum is: " + result);
+        }
+        static double Maximum(double x, double y, double z)
+        {
+            double maximumValue = x;
+
+            if (y > maximumValue)
+            {
+                maximumValue = y;
+            }
+            if (z > maximumValue)
+            {
+                maximumValue = z;
+            }
+
+            return maximumValue;
+
+        }
+    }
+}
